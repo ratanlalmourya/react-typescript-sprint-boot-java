e80317800ce10555c3ee4887755647718b2ff354
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-01 04:56:04 +0000

    Carousel created

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/navbar-and-footer/navbar';
 import { ExploreTopBooks } from './layouts/homepage/explore-top-books';
+import { Carousel } from './layouts/homepage/carsousel';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel></Carousel>
     </div>
   );
 }
