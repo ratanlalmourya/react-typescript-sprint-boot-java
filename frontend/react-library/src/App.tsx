e0880e766ad1e635e5dafc6fa3702753f4c62c09
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-01 06:26:46 +0000

    Footer created

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -4,6 +4,8 @@ import { Navbar } from './layouts/navbar-and-footer/navbar';
 import { ExploreTopBooks } from './layouts/homepage/explore-top-books';
 import { Carousel } from './layouts/homepage/carsousel';
 import { Heros } from './layouts/homepage/heros';
+import { LibraryServices } from './layouts/homepage/library-services';
+import { Footer } from './layouts/navbar-and-footer/footer';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <ExploreTopBooks />
       <Carousel />
       <Heros />
+      <LibraryServices />
+      <Footer />
     </div>
   );
 }
