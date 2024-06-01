353a286682a87f0e814b1c03d990d58a2c18700c
Ratan Mourya <68645660+ratanlalmourya@users.noreply.github.com>
2024-06-01 06:02:35 +0000

    Heros component created

--- frontend/react-library/src/App.tsx
+++ frontend/react-library/src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import { Navbar } from './layouts/navbar-and-footer/navbar';
 import { ExploreTopBooks } from './layouts/homepage/explore-top-books';
 import { Carousel } from './layouts/homepage/carsousel';
+import { Heros } from './layouts/homepage/heros';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
-      <Carousel></Carousel>
+      <Carousel />
+      <Heros />
     </div>
   );
 }
